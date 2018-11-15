e850b5a64e2a799979b665ced0d3d4922946ee5a
Alam <alamhasabie165@gmail.com>
2018-11-15 14:24:53 +0700

    toward 3D and beyond cukiwwww

--- graphic.py
+++ graphic.py
@@ -10,7 +10,15 @@ def init_window():
     display = (600,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     gluPerspective(45, (display[0]/display[1]), 0.1, 4000.0)
-    glTranslatef(0.0,0.0, -2000)
+    glTranslatef(0.0,0.0, -1500)
+
+def init_window_3d():
+    pygame.init()
+    display = (600,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 4000.0)
+    glTranslatef(0.0,0.0, -1500)
+    glRotatef(10,1,1,0)
 
 def render_polygon(CURRENT_VERTICES):
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
@@ -24,5 +32,22 @@ def render_polygon(CURRENT_VERTICES):
     glVertex3f(900,0,0)
     glVertex3f(0,-900,0)
     glVertex3f(0,900,0)
+    glEnd()
+    pygame.display.flip()
+
+def render_cube(CURRENT_VERTICES,EDGES):
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glBegin(GL_LINES)
+    for edge in EDGES :
+        for vertex in edge :
+            glVertex3f(CURRENT_VERTICES[vertex][0],CURRENT_VERTICES[vertex][1],CURRENT_VERTICES[vertex][2])
+    glEnd()
+    glBegin(GL_LINES)
+    glVertex3f(-900,0,0)
+    glVertex3f(900,0,0)
+    glVertex3f(0,-900,0)
+    glVertex3f(0,900,0)
+    glVertex3f(0,0,900)
+    glVertex3f(0,0,-900)
     glEnd()
     pygame.display.flip()
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 
 START_VERTICES = []
 CURRENT_VERTICES = []
+EDGES = []
+dimension = 0
 
 
 
@@ -20,42 +22,84 @@ def animate_translate_2d(dx,dy) :
 		i=i+1
 def animate_dilate(factor) :
 	global CURRENT_VERTICES
-	matrix = np.matrix(CURRENT_VERTICES)
-	output_matrix = dilate(matrix,factor)
-	diff= output_matrix - matrix
-	diff = diff/10
-	print(diff)
-	i=1
-	while i<=10:
-		matrix = matrix + diff
-		CURRENT_VERTICES = matrix.tolist()
-		render_polygon(CURRENT_VERTICES)
+	output_matrix = dilate(np.matrix(CURRENT_VERTICES),factor)
+	current_matrix = np.matrix(CURRENT_VERTICES)
+	CURRENT_VERTICES = output_matrix.tolist()
+	diff_matrix = output_matrix - current_matrix
+	diff_matrix = diff_matrix/60
+	i=0
+	while i<60:
+		current_matrix = current_matrix + diff_matrix
+		render_polygon(current_matrix.tolist())
 		i=i+1
+	render_polygon(CURRENT_VERTICES)
 def animate_rotate(command) :
 	global CURRENT_VERTICES
-	i = 1
+	i = 0
 	while i < int(command[1]) :
 		matrix = np.matrix(CURRENT_VERTICES)
 		matrix = rotate2D(matrix,1,float(command[2]),float(command[3]))
 		CURRENT_VERTICES = matrix.tolist()
 		render_polygon(CURRENT_VERTICES)
 		i=i+1
+	
 def animate_shear(command) :
 	global CURRENT_VERTICES
-	CURRENT_VERTICES = shear(np.matrix(CURRENT_VERTICES),command[1],float(command[2]),0).tolist()
+	matrix_current = np.matrix(CURRENT_VERTICES)
+	matrix_output = shear(matrix_current,command[1],float(command[2]),0)
+	CURRENT_VERTICES = matrix_output.tolist()
+	diff_matrix = matrix_output-matrix_current
+	diff_matrix=diff_matrix/120
+	i=0
+	while i<120 :
+		matrix_current = matrix_current + diff_matrix
+		render_polygon(matrix_current.tolist())
+		i=i+1
+	#CURRENT_VERTICES = shear(np.matrix(CURRENT_VERTICES),command[1],float(command[2]),0).tolist()
 	render_polygon(CURRENT_VERTICES)
 def animate_stretch(command) :
 	global CURRENT_VERTICES
-	CURRENT_VERTICES = stretch(np.matrix(CURRENT_VERTICES),command[1],float(command[2])).tolist()
+	output_matrix = stretch(np.matrix(CURRENT_VERTICES),command[1],float(command[2]))
+	current_matrix = np.matrix(CURRENT_VERTICES)
+	CURRENT_VERTICES = output_matrix.tolist()
+	diff_matrix = output_matrix - current_matrix
+	diff_matrix = diff_matrix/60
+	i=0
+	while i<60 :
+		current_matrix = current_matrix + diff_matrix
+		render_polygon(current_matrix.tolist())
+		i=i+1
 	render_polygon(CURRENT_VERTICES)
 def animate_reflect(command) :
 	global CURRENT_VERTICES
-	CURRENT_VERTICES = reflect(np.matrix(CURRENT_VERTICES),command[1],2).tolist()
+	output_matrix = reflect(np.matrix(CURRENT_VERTICES),command[1],2)
+	current_matrix = np.matrix(CURRENT_VERTICES)
+	CURRENT_VERTICES = output_matrix.tolist()
+	diff_matrix = output_matrix-current_matrix 
+	diff_matrix = diff_matrix/60
+	i=0
+	while i<60 :
+		current_matrix = current_matrix +diff_matrix
+		render_polygon(current_matrix.tolist())
+		i=i+1
 	render_polygon(CURRENT_VERTICES)
+
+
 def animate_custom(command) :
 	global CURRENT_VERTICES
-	CURRENT_VERTICES = custom(np.matrix(CURRENT_VERTICES),command,2).tolist()
+	output_matrix = custom(np.matrix(CURRENT_VERTICES),command,2)
+	current_matrix = np.matrix(CURRENT_VERTICES)
+	diff_matrix = output_matrix - current_matrix
+	CURRENT_VERTICES = output_matrix.tolist()
+	i=0
+	diff_matrix = diff_matrix/60
+	while i<60 :
+		current_matrix = current_matrix + diff_matrix
+		render_polygon(current_matrix.tolist())
+		i = i+1
 	render_polygon(CURRENT_VERTICES)
+
+
 def multiple(n_iterations) :
 	i=1
 	while(i<=n_iterations) :
@@ -80,18 +124,61 @@ def multiple(n_iterations) :
 		i=i+1
 
 
-
+def dimension3() :
+	global dimension
+	global START_VERTICES
+	global CURRENT_VERTICES
+	global EDGES
+	init_window_3d()
+	START_VERTICES = [
+		[-100.0,100.0,-100.0],
+		[100.0,100.0,-100.0],
+		[100.0,100.0,100.0],
+		[-100.0,100.0,100.0],
+		[-100.0,-100.0,-100.0],
+		[100.0,-100.0,-100.0],
+		[100.0,-100.0,100.0],
+		[-100.0,-100.0,100.0]
+	]
+	CURRENT_VERTICES = START_VERTICES
+	EDGES = [
+		[0,1],
+		[1,2],
+		[2,3],
+		[3,0],
+		[4,5],
+		[5,6],
+		[6,7],
+		[7,4],
+		[0,4],
+		[1,5],
+		[2,6],
+		[3,7]
+	]
+	render_cube(CURRENT_VERTICES,EDGES)
+	while True:
+		i=1
+	return 0
 
 
 def main():
+	global dimension
 	global CURRENT_VERTICES
 	global START_VERTICES
-	START_VERTICES = input_vertices()
-	CURRENT_VERTICES = START_VERTICES
-	init_window()
-	render_polygon(CURRENT_VERTICES)
-	is_running = True
-	while is_running :
+	while dimension!=2 and dimension !=3 :
+		dimension = int(input("Masukkan dimensi : "))
+		if dimension!=2 and dimension !=3 :
+			print("Saat ini program belum mampu menerima dimensi di luar nalar anda. Masukkan ulang : ")
+	if dimension ==3 :
+		dimension3()
+		is_running_2d = False
+	else :
+		is_running_2d = True
+		START_VERTICES = input_vertices()
+		CURRENT_VERTICES = START_VERTICES
+		init_window()
+		render_polygon(CURRENT_VERTICES)
+	while is_running_2d :
 		command = input('Masukkan perintah : ')
 		command = command.split(" ")
 		if command[0] == 'dilate' :
@@ -117,5 +204,4 @@ def main():
 			is_running = False
 		else :
 			print('Perintah salah. Masukkan perintah kembali :')
-
 main()
